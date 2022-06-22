481250e87bac7afddd5a08a1f7757f55d361141c
fidanmardanli <fidannm@code.edu.az>
2022-06-23 00:10:46 +0400

    Task about Classes

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace Class_Lesson { }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //string name1 = "Orxan";
+        //string surname1 = "Akberov";
+        //int age1 = 26;
+
+        //string name2 = "Emil";
+        //string surname2 = "Abdullayev";
+        //int age2 = 20;
+
+        //string name3 = "Fidan";
+        //string surname3 = "Mardanli";
+        //int age3 = 17;
+
+        //Console.WriteLine(name1 + " " + surname1);
+        
+
+
+        //var obj = new
+        //{
+        //    name = "Orxan",
+        //    surname = "Akberov",
+        //    age = 26
+
+        //};
+
+        //var obj2 = new
+        //{
+        //    name = "Fidan",
+        //    surname = "Mardanli",
+        //    age = 17
+        //};
+
+        //Console.WriteLine($"{obj2.name} {obj2.surname} Age: {obj2.age}");
+
+
+        //Student st1 = new Student("Fidan", 30 );
+        //st1.name = "Fidan";
+        //st1.surname = "Mardanli";
+        //st1.age = 17;
+
+        //Student st2 = new Student();
+        //st2.name = "Orxan";
+        //st2.surname = "Akberov";
+        //st2.age = 26;
+
+        //Console.WriteLine(st1.name);
+        //Console.WriteLine(st1.FullName());
+
+    //    Student student = new Student();
+    //    Student name = "Orxan";
+
+    //    var result = student.GetGroupInfo();
+
+
+    //    Console.WriteLine(result);
+
+
+
+    //}
+
+    //class Student
+    //{
+    //    public string name;
+    //    public int age;
+    //    public string surname;
+    //    public string adress;
+
+
+    //    public Student()
+    //    {
+    //        Console.WriteLine("empty");
+    //    }
+
+    //    public Student(int age, string adress):this(adress)
+    //    {
+    //        this.adress = adress;
+    //        Console.WriteLine("Age : " + age);
+    //    }
+            
+    //    public Student(string adds):this(true) 
+    //    {
+    //        Console.WriteLine("Adress :" + adds);
+    //    }
+
+    //    public Student(bool isMarried)
+    //    {
+    //        Console.WriteLine("is married");
+    //    }
+
+
+
+
+        //public Student(string studentName, int studentAge)
+        //{
+        //    name = studentName;
+        //    age = studentAge;
+        //    Console.WriteLine(name);
+        //}
+
+
+
+        //public Student(string studentName)
+        //{
+        //    name = studentName;
+        //    Console.WriteLine(name);
+        //}
+
+        //public Student()
+        //{
+        //    Console.WriteLine("Constructor");
+        //}
+
+        //public string FullName(string adress)
+        //{
+        //    return name + " " + surname + " " + adress;
+        //}
+
+        //public string FullData()
+        //{
+        //    return name + " " + surname + "" + age;
+        //}
+
+
+//        public string GetGroupInfo()
+//        {
+//            Teacher teacher = new Teacher();
+//            teacher.name = "cavid";
+
+//            return $"Teacher name: {teacher.GetTeacherName()} Student name: {name}"
+
+//        }
+//    }
+
+//    class Teacher
+//    {
+//        public string name;
+//        public decimal salary;
+        
+//        public string GetTeacherName()
+//        {
+//            return name;
+//        }
+//    }
+//}
